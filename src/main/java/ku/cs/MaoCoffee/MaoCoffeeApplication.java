e13d5502a1b59370c3f0eac2bc5685041a8f8087
Thanapat Butsai <thanapatbutsai@Thanapats-MacBook-Pro.local>
2023-08-27 22:52:38 +0700

    Lab 5

---
+++ src/main/java/ku/cs/MaoCoffee/MaoCoffeeApplication.java
@@ -0,0 +1,13 @@
+package ku.cs.MaoCoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaoCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaoCoffeeApplication.class, args);
+	}
+
+}
